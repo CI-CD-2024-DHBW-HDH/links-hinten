49202f4f2f38d1f2ce615c1e39d2085b3b14b4ca
Brilmayer, Paul - TIN2021 <brilmayerp.tin21@student.dhbw-heidenheim.de>
2024-02-26 11:11:37 +0100

    implemented medium bot move

--- src/logic/bots/medium.ts
+++ src/logic/bots/medium.ts
@@ -6,7 +6,35 @@ import { randomMove, winningMove } from "./bot";
 // or plays the center field if it can
 // otherwise it plays a random move
 export function mediumMove(board: Field[], own: Field): number {
-  return -1
+  const otherPlayer = (own === Field.PLAYER1) ? Field.PLAYER2 : Field.PLAYER1;
+  const opponentWinMove = winningMove(board, otherPlayer);
+  const ownWinMove = winningMove(board, own);
+
+  const blankFields = getBlanks(board);
+  if (blankFields.length === 0) {
+    return -1;
+  }
+
+  if (ownWinMove !== -1) {
+    board[ownWinMove] = own;
+    return ownWinMove;
+  }else if (opponentWinMove !== -1) {
+    board[opponentWinMove] = own;
+    return opponentWinMove;
+  } else if (board[4] === Field.EMPTY) {
+    board[4] = own;
+    return 4;
+  } else {
+    let blankFields = getBlanks(board);
+    let rndMove = randomMove(blankFields.length);
+
+    while (board[rndMove] !== Field.EMPTY) {
+      rndMove = randomMove(blankFields.length);
+    }
+
+    board[rndMove] = own;
+    return rndMove
+  }
 }
 
 // this bot just tries to block a win
