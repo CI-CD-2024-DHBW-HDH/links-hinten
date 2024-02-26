256d49166a647eacfe92bf6dd1f52f06a1d275a9
MHummel <mhummel@artiso.com>
2024-02-26 13:29:03 +0100

    fix liter

--- src/logic/bots/hard.ts
+++ src/logic/bots/hard.ts
@@ -1,51 +1,53 @@
-import { Field, getBlanks, invertPlayer, won } from '../game'
+import { Field, getBlanks, invertPlayer, won } from "../game";
 
-export function hardMove (board: Field[], own: Field): number {
-  const blanks = getBlanks(board)
+export function hardMove(board: Field[], own: Field): number {
+  const blanks = getBlanks(board);
 
-  const moves = new Map<number, number>()
+  const moves = new Map<number, number>();
   for (const move of blanks) {
-    const copyBoard = [...board]
-    copyBoard[move] = own
-    moves.set(move, minimax(copyBoard, invertPlayer(own), own, false))
-    copyBoard[move] = Field.EMPTY
+    const copyBoard = [...board];
+    copyBoard[move] = own;
+    moves.set(move, minimax(copyBoard, invertPlayer(own), own, false));
+    copyBoard[move] = Field.EMPTY;
   }
 
-  let bestScore: [number, number] = [-1, -Infinity]
+  let bestScore: [number, number] = [-1, -Infinity];
   for (const move of moves) {
-    if (move[1] > bestScore[1]) bestScore = move
+    if (move[1] > bestScore[1]) bestScore = move;
   }
 
-  return bestScore[0]
+  return bestScore[0];
 }
 
-function minimax (
+function minimax(
   board: Field[],
   currentPlayer: Field,
   own: Field,
-  maximize: boolean
+  maximize: boolean,
 ): number {
-  const blanks = getBlanks(board)
+  const blanks = getBlanks(board);
 
-  const winner = won(board)
-  if (winner === own) return 1 // win
-  else if (winner === invertPlayer(own)) return -1 // loss
-  else if (blanks.length === 0) return 0 // draw
+  const winner = won(board);
+  if (winner === own)
+    return 1; // win
+  else if (winner === invertPlayer(own))
+    return -1; // loss
+  else if (blanks.length === 0) return 0; // draw
 
-  let bestScore: number = maximize ? -Infinity : Infinity
+  let bestScore: number = maximize ? -Infinity : Infinity;
 
   // let copyBoard = [...board]
   for (const move of blanks) {
-    board[move] = currentPlayer
+    board[move] = currentPlayer;
     if (maximize) {
-      const score = minimax(board, invertPlayer(currentPlayer), own, false)
-      bestScore = Math.max(bestScore, score)
+      const score = minimax(board, invertPlayer(currentPlayer), own, false);
+      bestScore = Math.max(bestScore, score);
     } else {
-      const score = minimax(board, invertPlayer(currentPlayer), own, true)
-      bestScore = Math.min(bestScore, score)
+      const score = minimax(board, invertPlayer(currentPlayer), own, true);
+      bestScore = Math.min(bestScore, score);
     }
-    board[move] = Field.EMPTY
+    board[move] = Field.EMPTY;
   }
 
-  return bestScore
+  return bestScore;
 }
