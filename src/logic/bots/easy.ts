4fe272d6ffbffd6a3eabc077e9641ba545c1be55
Tobias703 <seiffertt.tin21@student.dhbw-heidenheim.de>
2024-02-26 10:05:38 +0100

    Partially added easy move

--- src/logic/bots/easy.ts
+++ src/logic/bots/easy.ts
@@ -1,7 +1,16 @@
 import { Field } from "../game";
+import { winningMove } from "./bot";
 
 // the easy bot plays a winning move, if it can
 // otherwise, it plays a random move
 export function easyMove(board: Field[], own: Field): number {
-  return -1
+  //TODO: Check for winning move
+  const emptyPositions: number[] = [];
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] === Field.EMPTY) {
+            emptyPositions.push(i);
+        }
+    }
+    const randomIndex = Math.floor(Math.random() * emptyPositions.length);
+    return emptyPositions[randomIndex];
 }
